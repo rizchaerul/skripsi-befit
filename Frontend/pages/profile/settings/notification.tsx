1d4e2178a036d03364d0c343cc2975296acbf714
Chaerul Rizky <riz.chaerul@gmail.com>
2022-09-04 17:33:53 +0700

    fix: reload after click

--- Frontend/pages/profile/settings/notification.tsx
+++ Frontend/pages/profile/settings/notification.tsx
@@ -150,6 +150,8 @@ const NotificationPage: NextPageWithLayout = () => {
                                         JSON.stringify(sub)
                                     );
                                 }
+
+                                window.location.reload();
                             }}
                         >
                             Click here
